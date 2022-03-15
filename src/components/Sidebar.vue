a95e70e4d413f4f05de67174d97685b3f4dda05e
Hongbusi <q951686662@qq.com>
2022-03-15 20:33:57 +0800

    chore: update

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,10 +1,8 @@
 <template>
   <aside class="sidebar">
     <nav class="sidebar-nav">
-      <a v-for="item in sidebar" :key="item" class="link" :href="`#${item}`">
-        <p class="link-text">
-          {{ item }}
-        </p>
+      <a v-for="item in sidebar" :key="item" class="link active" :href="`#${item}`">
+        {{ item }}
       </a>
     </nav>
   </aside>
@@ -23,7 +21,7 @@ const sidebar = Object.keys(data)
   top: var(--hbs-header-height);
   bottom: 0;
   left: 0;
-  padding: 0 32px 96px calc((100% - var(--hbs-max-width)) / 2 + var(--hbs-blank-width));
+  padding: 0 var(--hbs-blank-width) 96px calc((100% - var(--hbs-max-width)) / 2 + var(--hbs-blank-width));
   width: calc((100% - var(--hbs-max-width)) / 2 + var(--hbs-sidebar-width));
   height: 100%;
   background-color: var(--docs-c-bg);
@@ -36,23 +34,39 @@ const sidebar = Object.keys(data)
 
 @media (max-width: 1440px) {
   .sidebar {
-    /* padding: 0 32px 96px calc((100% - var(--vp-screen-max-width)) / 2); */
     display: none;
   }
 }
 
 .link {
+  position: relative;
   display: block;
-  padding: 8px 0;
-}
-
-.link-text {
-  line-height: 20px;
+  padding-left: var(--hbs-blank-width);
+  padding: 6px 12px;
+  margin-top: 4px;
+  margin-bottom: 8px;
   transition: color .25s;
+  opacity: 0.66;
 }
 
-.link.active .link-text,
-.link:hover .link-text {
-  color: var(--docs-c-red);
+.link.active {
+  color: var(--hbs-c-text-primary);
+  background-color: var(--hbs-c-bg-primary-light);
+}
+
+.link.active:hover {
+  color: var(--hbs-c-text-primary);
+}
+
+.link.active::before {
+  position: absolute;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  content: "";
+  transform: scaleY(1);
+  opacity: 1;
+  transition: transform .15s cubic-bezier(.645, .045, .355, 1), opacity .15s cubic-bezier(.645, .045, .355, 1);
+  border-right: 3px solid var(--hbs-c-bg-primary);
 }
 </style>
