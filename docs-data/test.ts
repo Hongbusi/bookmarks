b0266c96856a63c576bd7e78ca0cb77a3c3c942c
Hongbusi <q951686662@qq.com>
2022-03-23 10:27:45 +0800

    chore: useDocsData

---
+++ docs-data/test.ts
@@ -0,0 +1,10 @@
+export default [
+  {
+    name: 'Vue3',
+    logo: 'https://v3.cn.vuejs.org/logo.png',
+    desc: '这是一个描述',
+    zhLink: 'https://v3.cn.vuejs.org',
+    enLink: 'https://vuejs.org',
+    repo: 'vuejs/core'
+  }
+]
--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,14 @@
 import type { ViteSSGContext } from 'vite-ssg'
 
 export type UserModule = (ctx: ViteSSGContext) => void
+
+export interface DocsDataItem {
+  logo: string
+  name: string
+  desc: string
+  zhLink: string
+  enLink: string
+  repo: string
+}
+
+export type DocsData = Record<string, DocsDataItem[]>
