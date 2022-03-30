30d2edef53705c094c02bddf4c72854daeb073d7
Hongbusi <q951686662@qq.com>
2022-03-30 22:29:53 +0800

    chore: update

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -5,7 +5,7 @@
         <h2 class="text-lg">
           Bookmarks
         </h2>
-        <a class="text-sm" href="https://github.com/Hongbusi/docs-navigation" target="_blank">
+        <a class="text-sm transition-all hover:text-[#de3a31]" href="https://github.com/Hongbusi/docs-navigation" target="_blank">
           参与贡献/建议
         </a>
       </nav>
@@ -15,6 +15,7 @@
 
     <footer class="container pt-16px pb-30px mx-auto text-center text-gray-600 text-sm">
       Copyright (c) 2022 Hongbusi.
+      <a class="ml-8px transition-all hover:text-[#de3a31]" href="https://docschina.org" target="_blank">Design - 印记中文</a>
     </footer>
   </main>
 
