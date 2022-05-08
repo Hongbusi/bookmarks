5bde21fcd397019819611e0223e9f69acfdbfb66
unonu <594zhaogaosheng@gmail.com>
2022-05-08 17:37:43 +0800

    feat: back to top (#2)
    * Update issue templates
    
    * feat: add go to top button
    
    * style: optimize style
    
    optimize scroll bar style
    
    * ci: remove file
    
    remove issue template
    
    * style: remove root
    
    * style: remove hsl
    
    Co-authored-by: zhao.gaosheng <zhao.gaosheng@nint.com>

---
+++ src/components/GoTop.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+const { y } = useWindowScroll()
+
+const goTop = () => {
+  document.documentElement.scrollTop = document.body.scrollTop = 0
+}
+</script>
+
+<template>
+  <a v-if="y>200" class="fixed bottom-10 right-10 bg-blue-500 dark:bg-blue-700 rounded-lg p-2" @click="goTop">
+    <div i-akar-icons-align-to-top class="text-3xl text-white dark:text-opacity-80" />
+  </a>
+</template>
